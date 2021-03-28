e793c915a8c9f70093c494570104f6a039f4f09f
Alibek <sayakalibek1@gmail.com>
2021-03-28 19:39:11 +0600

    style: Sidebar component

--- src/components/atoms/Sidebar/index.tsx
+++ src/components/atoms/Sidebar/index.tsx
@@ -2,15 +2,23 @@ import { Link as ReactLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Div = styled.div`
-  background: gray;
+  background: white;
   width: 260px;
-  height: 100vh;
+  height: auto;
 `;
 
 const Link = styled(ReactLink)`
   display: block;
   font-size: 16px;
-  padding: 10px 20px;
+  padding: 20px;
+  padding-left: 30px;
+  margin-left: 10px;
+  text-decoration: none;
+  color: gray;
+  background: url('petroleum.svg');
+  background-position: left;
+  background-size: 20px;
+  background-repeat: no-repeat;
 `;
 
 export default function Sidebar(): JSX.Element {
